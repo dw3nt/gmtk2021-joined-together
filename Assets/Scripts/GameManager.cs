f8f09c1a065b6f209dd79086f04253217cbd8b4e
dw3nt <rick@dualwieldent.com>
2021-06-12 11:08:31 -0700

    points and timer displays

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Transform pointsLabel;
+    [SerializeField] private Transform timerLabel;
+    [SerializeField] private float startTime = 30f;
+
     public static GameManager instance;
     private int points = 0;
+    private float timeRemaining;
+    private bool timerRunning = true;
+
+    private Text pointsText;
+    private Text timerText;
 
     void Awake()
     {
         if (instance == null) {
             instance = this;
+            pointsText = pointsLabel.GetComponent<Text>();
+            timerText = timerLabel.GetComponent<Text>();
+            timeRemaining = startTime;
         } else {
             Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (timerRunning) {
+            timeRemaining -= Time.deltaTime;
+            timerText.text = "Time remaining: " + Mathf.FloorToInt(timeRemaining % 60);
+
+            if (timeRemaining <= 0) {
+                timerRunning = false;
+                timerText.text = "Time remaining: 0";
+                Debug.Log("level ended");
+                // play buzzer?
+                // spawn in menu
+            }
         }
     }
 
     public void AddPoints(int _points)
     {
         points += _points;
-        Debug.Log(points);
+        pointsText.text = "Matches: " + points;
+        // play sound for match
     }
 }
