abaff49b9a96fb7de31f6c8e03e9ae8828191674
dw3nt <rick@dualwieldent.com>
2021-06-12 23:03:34 -0700

    enemy spawner

---
+++ Assets/Scripts/NpcMatchSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcMatchSpawner : MonoBehaviour
+{
+    const int SCREEN_BOUNDS = 10;
+
+    void Awake()
+    {
+        int matchCount = Random.Range(5, transform.childCount);
+
+        for (int i = 0; i < matchCount; i++) {
+            Transform child = gameObject.transform.GetChild(Random.Range(0, transform.childCount));
+            if (child.gameObject.activeSelf) {
+                i--;
+            }
+            child.gameObject.SetActive(true);
+
+            child.GetChild(0).transform.position = new Vector2(
+                Random.Range(-SCREEN_BOUNDS, SCREEN_BOUNDS),
+                Random.Range(-SCREEN_BOUNDS, SCREEN_BOUNDS)
+            );
+            child.GetChild(1).transform.position = new Vector2(
+                Random.Range(-SCREEN_BOUNDS, SCREEN_BOUNDS),
+                Random.Range(-SCREEN_BOUNDS, SCREEN_BOUNDS)
+            );
+        }
+    }
+}
